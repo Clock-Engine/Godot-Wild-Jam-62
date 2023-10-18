b72c6bde0748210df005e7f7b9b99752332291b6
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-10-18 16:57:06 -0300

    Animação de dash do jogador implementada

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var doingAction = false
+
 @onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
@@ -21,13 +23,22 @@ func _physics_process(delta):
 		get_node("AnimatedSprite2D").flip_h = true
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
-	if direction:
+	if direction && doingAction == false:
 		if velocity.y == 0:
 			anim.play("Run")
 		velocity.x = direction * SPEED
 	else:
-		if velocity.y == 0:
+		if velocity.y == 0 && doingAction == false:
 			anim.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	if Input.is_action_just_pressed("dash"):
+		doingAction = true
+		anim.play("Dash")
+		velocity.x = move_toward(1000, 0, 10) #################
+		
 	move_and_slide()
+
+
+func _on_animation_player_animation_finished(Dash):
+	doingAction = false # Replace with function body.
