d7fd3100787b2615260a03bbd7e70742f76d177e
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-10-18 11:06:24 -0300

    Animações de movimento do jogador implementadas

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,22 +7,27 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
 	if direction:
+		if velocity.y == 0:
+			anim.play("Run")
 		velocity.x = direction * SPEED
 	else:
+		if velocity.y == 0:
+			anim.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
