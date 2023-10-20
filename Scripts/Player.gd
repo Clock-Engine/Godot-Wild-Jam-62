031b48e38cabede565d496f78bbc52fea4e9fc1f
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-10-20 17:55:35 -0300

    Ataque e dash modificados

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,31 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
+const DASH_SPEED = 1500.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 var doingAction = false
+var last_direction = 1
 
 @onready var anim = get_node("AnimationPlayer")
+@onready var action_timer = Timer.new()
+
+func _ready():
+	add_child(action_timer)
+	action_timer.wait_time = 1
+	action_timer.one_shot = true
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction == -1:
+	if direction == -1 && doingAction == false:
+		last_direction = -1
 		get_node("AnimatedSprite2D").flip_h = true
-	elif direction == 1:
+	elif direction == 1 && doingAction == false:
+		last_direction = 1
 		get_node("AnimatedSprite2D").flip_h = false
 	if direction && doingAction == false:
 		if velocity.y == 0:
@@ -35,22 +39,21 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	if Input.is_action_just_pressed("dash"):
-		doingAction = true
-		anim.set_speed_scale(3)
-		anim.play("Dash")
-		if doingAction == true:
-			velocity.x = direction * SPEED
-			print("OK")
-		get_tree().create_timer(1)
+		if doingAction == false:
+			doingAction = true
+			anim.set_speed_scale(4)
+			anim.play("Dash")
+			velocity.x = last_direction * DASH_SPEED
+			move_and_slide()
 		
 	if Input.is_action_just_pressed("attack"):
-		doingAction = true
-		anim.set_speed_scale(3)
-		anim.play("Attack1")
-		get_tree().create_timer(1)
+		if doingAction == false:
+			doingAction = true
+			anim.set_speed_scale(3)
+			anim.play("Attack1")
+			move_and_slide()
 		
 	move_and_slide()
 
-
 func _on_animation_player_animation_finished(Dash):
-	doingAction = false # Replace with function body.
+	doingAction = false
